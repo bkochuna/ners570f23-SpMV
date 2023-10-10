94f85c07b3dd1b2aea80001942c3ee6c66a69190
Brendan Kochunas <bkochuna@umich.edu>
2023-10-10 16:39:22 -0400

    Small modifications for Lec13
    Description:
    - Updated build system for moving header (oops!)
    - Updated class to use DBC and builder pattern.
    - Updated constructor to take sizes
    - Updated minimum working example

--- examples/mwe.cpp
+++ examples/mwe.cpp
@@ -7,8 +7,12 @@ int main(int argc, char* argv[])
 {
   cout << "Hello World!" << endl;
 
-  SpMV::SparseMatrix* ptr_A = new SpMV::SparseMatrix();
-
+  SpMV::SparseMatrix* ptr_A = new SpMV::SparseMatrix(1,5);
+  
+  // New scoping unit. This means variables defined in here, stay here.
+  {
+    SpMV::SparseMatrix A = SpMV::SparseMatrix(2,2);
+  }
 
   delete(ptr_A);
 
--- spmv/include/SparseMatrix.hpp
+++ spmv/include/SparseMatrix.hpp
@@ -1,25 +1,55 @@
 #pragma once
 
+#include <cstddef>
+#include <cassert>
 #include <iostream>
 
 namespace SpMV
 {
+    // Enumerations for matrix state to support Builder Pattern
+    enum MatrixState { undefined, initialized, building, assembled };
+
     class SparseMatrix
     {
-    private: 
-        /* put our attributes here */
+    protected: 
+        size_t _ncols = 0;
+        size_t _nrows = 0;
+        size_t _nnz   = 0;
+
+        MatrixState _state = undefined;
+
     public:
-        SparseMatrix(/* args go here */);
+        SparseMatrix(const int nrows, const int ncols);
        ~SparseMatrix();
+
+       void setCoefficient(const size_t row, const size_t col, const double aij);
     };
 
-    SparseMatrix::SparseMatrix(/* args go here */)
+    SparseMatrix::SparseMatrix(const int nrows, const int ncols) :
+        _nrows(nrows), _ncols(ncols)
     {
+        assert(this->_state == undefined);
         std::cout << "Hello from SparseMatrix Constructor!" << std::endl;
+        std::cout << "Size is (nrows,ncols) = (" 
+            << this->_nrows << "," << this->_ncols << ")" << std::endl;
+        this->_state = initialized;
+        assert(this->_state == initialized);
     }
 
     SparseMatrix::~SparseMatrix()
     {
         std::cout << "Hello from SparseMatrix Destructor!" << std::endl;
+    }
+
+    void SparseMatrix::setCoefficient(const size_t row, const size_t col, const double aij)
+    {
+        assert(this->_state != undefined);
+
+        this->_state = building;
+
+        //Store value
+        //Increment nnz
+
+        assert(this->_state == building);
     }
 }

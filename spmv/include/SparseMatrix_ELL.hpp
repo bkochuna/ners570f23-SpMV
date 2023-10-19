4ccece5b188212ade344d00bc97ea91306db2ada
Prathamesh Nakhate <pnakhate@gl-login2.arc-ts.umich.edu>
2023-10-19 17:47:59 -0400

    Added the ELL Matrix class
    Description:
    Added default constructor and the constructor specific to ELL. Also added setCoefficient() for ELL to assemble colIdx and val

--- spmv/include/SparseMatrix_ELL.hpp
+++ spmv/include/SparseMatrix_ELL.hpp
@@ -9,15 +9,18 @@ namespace SpMV
     class SparseMatrix_ELL : public SparseMatrix<fp_type>
     {
     private:
-        size_t *I    = nullptr;
-        size_t *J    = nullptr;
+        // size_t *I    = nullptr;
+        // size_t *J    = nullptr;
+        size_t *colIdx = nullptr;
         fp_type *val = nullptr;
-        string format = nullptr;
+        size_t _nrowsmax;
+        //string format = "ELL";
 
     public:
-        SparseMatrix_COO(const int nrows, const int ncols);
+        SparseMatrix_ELL(const int nrows, const int ncols);
+        void setCoefficient(const size_t row, const size_t col, const fp_type aij);
         void assembleStorage() {};
-        void getCoef(size_t i,size_t j);
-        string getFormat();
+        //void getCoef(size_t i,size_t j);
+        //string getFormat();
     };
 }
--- spmv/src/SparseMatrix_ELL.cpp
+++ spmv/src/SparseMatrix_ELL.cpp
@@ -1,1 +1,41 @@
 #include "SparseMatrix_ELL.hpp"
+
+#include<iostream>
+
+namespace SpMV
+{
+    template <class fp_type>
+    SparseMatrix_ELL<fp_type>::SparseMatrix_ELL(const int nrows, const int ncols) :
+         SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+    {
+        std::cout << "Hello from SparseMatrix_ELL Constructor!" << std::endl;
+        this->colIdx = new size_t[this->_nrowsmax*this->_nrows];
+        this->val = new size_t[this->_nrowsmax*this->_nrows];
+        for (size_t i = 0; i < this->_nrowsmax*this->_nrows; i++)
+        {
+            colIdx[i]=-1; // -1 corresponds to the zero entries
+            val[i]=-1;
+        }  
+        
+    }
+
+    template <class fp_type>
+    void SparseMatrix_ELL<fp_type>::setCoefficient(const size_t row, const size_t col, const fp_type aij){
+
+        // First call the sefCoeff from SparseMatrix
+        SparseMatrix<fp_type>::setCoefficient(row,col,aij);
+
+        // Now assign the entry to colIdx and val
+        for (size_t i = 0; i < this->_nrowsmax; i++)
+        {
+            if(colIdx[this->_nrows*i+row]==-1){
+                colIdx[this->_nrows*i+row]=col;
+                val[this->_nrows*i+row]=aij;
+                break;
+            }
+        }  
+    }   
+
+    template class SparseMatrix_ELL<float>;
+    template class SparseMatrix_ELL<double>;
+}

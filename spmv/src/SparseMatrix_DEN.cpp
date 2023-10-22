a9a06caeebe83bcc9285e8a21145fa1e866b6939
nzamb <nzamb@umich.edu>
2023-10-22 16:06:59 -0400

    added DEN constructor/destructor

---
+++ spmv/src/SparseMatrix_DEN.cpp
@@ -0,0 +1,17 @@
+#include "SparseMatrix_DEN.hpp"
+
+#include <iostream>
+
+namespace SpMV
+{
+    template <class fp_type>
+    SparseMatrix_DEN<fp_type>::SparseMatrix_DEN(const int nrows, const int ncols) :
+         SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+    {
+        std::cout << "Hello from SparseMatrix_DEN Constructor!" << std::endl;
+    }
+
+
+
+    template class SparseMatrix_DEN<float>;
+}

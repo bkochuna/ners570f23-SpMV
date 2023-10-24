998e7f290e3021d1845d1b354063ad08874d59e9
Think <Think1@jiazikangdeMacBook-Pro.local>
2023-10-23 22:40:34 -0400

    CSR_view method is modified

--- spmv/src/SparseMatrix_CSR.cpp
+++ spmv/src/SparseMatrix_CSR.cpp
@@ -1,6 +1,7 @@
 #include "SparseMatrix_CSR.hpp"
 
 #include <iostream>
+#include <fstream> 
 using namespace std;
 
 namespace SpMV
@@ -150,6 +151,37 @@ namespace SpMV
             }
         }
     }
+
+    template <class fp_type>
+void SparseMatrix_CSR<fp_type>::CSR_view(const std::string& filename) {
+    std::ofstream outputFile(filename);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open the file for writing." << std::endl;
+        return;
+    }
+    
+    outputFile << "[ ";
+    for (int i = 0; i < this->_nrows; i++) {
+        for (int j = 0; j < this->_ncols; j++) {
+            int columnIdx = -1;
+            for (size_t k = this->rowPtrs[i]; k < this->rowPtrs[i + 1]; k++) {
+                if (this->colIdx[k] == j) {
+                    columnIdx = k;
+                    break;
+                }
+            }
+            if (columnIdx != -1) {
+                outputFile << this->value[columnIdx] << " ";
+            } else {
+                outputFile << "0 ";
+            }
+        }
+        outputFile << std::endl;
+    }
+    outputFile << "]";
+
+    outputFile.close();
+}
 
     template class SparseMatrix_CSR<float>;
     template class SparseMatrix_CSR<double>;

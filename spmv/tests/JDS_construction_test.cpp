7b06c938c5a818e33f754c6a6cdf60d3ad75a2d8
Rebecca E Grawe <rgrawe@umich.edu>
2023-10-26 16:12:30 -0400

    Removed .getFormat() check from JDS constructor test

--- spmv/tests/JDS_construction_test.cpp
+++ spmv/tests/JDS_construction_test.cpp
@@ -18,20 +18,12 @@ TEST_CASE(matrix_col_size) {
     ASSERT(matrix.getNumCols() == static_cast<size_t>(ncols));
 }
 
-template <typename T>
-TEST_CASE(matrix_type) {
-    int nrows = 10;
-    int ncols = 20;
-    SpMV::SparseMatrix_JDS<T> matrix = SpMV::SparseMatrix_JDS<T>(nrows, ncols);
-    ASSERT(matrix.getFormat() == "JDS");
-}
 
 // Create a test suite
 template <typename T>
 TEST_SUITE(construction) {
     TEST(matrix_row_size<T>);
     TEST(matrix_col_size<T>);
-    TEST(matrix_type<T>);
 }
 
 auto

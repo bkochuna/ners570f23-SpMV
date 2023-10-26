ee4d2576605a5fc01ae64ca7c4c8005398c1a7f3
Prathamesh Nakhate <pnakhate@gl-login2.arc-ts.umich.edu>
2023-10-25 23:17:25 -0400

    Added templating to matvec test

--- spmv/tests/ELL_matvec_ut.cpp
+++ spmv/tests/ELL_matvec_ut.cpp
@@ -7,11 +7,11 @@ TEST_CASE(empty_in){
     try
     {
         SpMV::SparseMatrix<fp_type>* test_matrix = new SpMV::SparseMatrix_ELL<fp_type>(3,3);
-        test_matrix->setCoefficient(0,0,1.0);
-        test_matrix->setCoefficient(1,1,2.0);
-        test_matrix->setCoefficient(2,2,3.0);
-        test_matrix->setCoefficient(1,2,4.0);
-        test_matrix->setCoefficient(2,0,5.0);
+        test_matrix->setCoefficient(0,0,static_cast<fp_type>(1.0));
+        test_matrix->setCoefficient(1,1,static_cast<fp_type>(2.0));
+        test_matrix->setCoefficient(2,2,static_cast<fp_type>(3.0));
+        test_matrix->setCoefficient(1,2,static_cast<fp_type>(4.0));
+        test_matrix->setCoefficient(2,0,static_cast<fp_type>(5.0));
 
         test_matrix->assembleStorage();
 
@@ -24,15 +24,15 @@ TEST_CASE(empty_in){
 template <typename fp_type>
 TEST_CASE(vec_in){
     SpMV::SparseMatrix<fp_type>* test_matrix = new SpMV::SparseMatrix_ELL<fp_type>(3,3);
-    test_matrix->setCoefficient(0,0,1.0);
-    test_matrix->setCoefficient(1,1,2.0);
-    test_matrix->setCoefficient(2,2,3.0);
-    test_matrix->setCoefficient(1,2,4.0);
-    test_matrix->setCoefficient(2,0,5.0);
+    test_matrix->setCoefficient(0,0,static_cast<fp_type>(1.0));
+    test_matrix->setCoefficient(1,1,static_cast<fp_type>(2.0));
+    test_matrix->setCoefficient(2,2,static_cast<fp_type>(3.0));
+    test_matrix->setCoefficient(1,2,static_cast<fp_type>(4.0));
+    test_matrix->setCoefficient(2,0,static_cast<fp_type>(5.0));
 
     test_matrix->assembleStorage();
 
-    fp_type input[3]={2.0,20.0,-5.0};
+    fp_type input[3]={static_cast<fp_type>(2.0),static_cast<fp_type>(20.0),static_cast<fp_type>(-5.0)};
     fp_type output[3];
     test_matrix->matvec(input,output);
     for (size_t i = 0; i < 3; i++)

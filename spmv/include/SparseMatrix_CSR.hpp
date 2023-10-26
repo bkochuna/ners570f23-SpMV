17152536808418bb8e9b978ad0d87e9b81fe5635
maxzog <maxzog@umich.edu>
2023-10-26 08:49:46 -0400

    Assemble test compiles. Added declaration of disassmbleStorage to header

--- spmv/include/SparseMatrix_CSR.hpp
+++ spmv/include/SparseMatrix_CSR.hpp
@@ -26,8 +26,8 @@ namespace SpMV
         void setCoefficient(const size_t row, const size_t col, const fp_type aij);
         void CSR_view(const std::string& filename);
         void assembleStorage() {};
+        void disassembleStorage() {};
         void getCoef(size_t i, size_t j, fp_type* Val);
-        std::string getFormat();
 	     void matvec(fp_type* vecin, fp_type* vecout);
     };
 }

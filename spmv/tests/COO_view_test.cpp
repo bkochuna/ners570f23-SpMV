7881ab9f1421f495796d971c58209c668382ea53
zkjiaumich <zkjia@umich.edu>
2023-10-26 15:18:59 -0400

    COO_view_test is modified

--- spmv/tests/COO_view_test.cpp
+++ spmv/tests/COO_view_test.cpp
@@ -1,71 +1,91 @@
+#include "SpMV.hpp"
+#include "SparseMatrix_COO.hpp"
 #include <iostream>
+#include <fstream>
 #include <sstream>
-#include <SpMV.hpp>
 #include "unit_test_framework.hpp"
 
+using namespace std;
 
 // Create a unit test
 template <typename T>
 TEST_CASE(Diagonal) {
-    size_t nrows = 4;
-    size_t ncols = 4;
-    SpMV::SparseMatrix_COO<T> matrix = SparseMatrix_COO<T>(nrows, ncols);
-    matrix.setCoefficient(0, 0, 2.0);
-    matrix.setCoefficient(1, 1, 3.0);
-    matrix.setCoefficient(2, 2, -6.5);
-    matrix.setCoefficient(3, 3, -8.1);
-
-    std::stringstream buffer;
-    std::streambuf * old = std::cout.rduf(buffer.rduf());
+    SpMV::SparseMatrix_COO<T> matrix = SpMV:: SparseMatrix_COO<T>(4, 4);
+    matrix.setCoefficient(0, 0, T(2.0));
+    matrix.setCoefficient(1, 1, T(3.0));
+    matrix.setCoefficient(2, 2, T(-6.5));
+    matrix.setCoefficient(3, 3, T(-8.1));
 
     matrix.COO_view();
-    
-    std:: string text = buffer.str();
-    std::cerr << "The string is: " << text << std:endl;
 
-    std::string text_ref = "2.0 0.0 0.0 0.0 \n 0.0 3.0 0.0 0.0 \n 0.0 0.0 -6.5 0.0 \n 0.0 0.0 0.0 -8.1";
+    int idx[4] = {0, 1, 2, 3};
+    int jdx[4] = {0, 1, 2, 3};
+    T rval[4] = {T(2.0), T(3.0), T(-6.5), T(-8.1)};
 
-    if (text == text_ref){
-        std::cerr << "Test passed" << std:endl;
-    } else {
-        std::cerr << "Test failed" << std::endl;
-        return 1;
+    string reference;
+    int k = 0;
+    int i = 0;
+
+    T value;
+    ifstream ViewResults("./COO.out");
+    while (getline(ViewResults, reference)){
+
+        int j = 0;
+        istringstream iss(reference);
+        while(iss >> value){
+            if (i==idx[k] && j==jdx[k]){
+                ASSERT_NEAR(value, T(rval[k]), T(1.E-6));
+                k++;
+            } else {
+                ASSERT_NEAR(value, T(0.), T(1.E-6));
+            }
+            j++;
+        }
+
+        i++;
     }
 
-    return 0;
 }
 
 template <typename T>
 TEST_CASE(Uppertri) {
-    size_t nrows = 3;
-    size_t ncols = 3;
-    SpMV::SparseMatrix_COO<T> matrix = SparseMatrix_COO<T>(nrows, ncols);
-    matrix.setCoefficient(0, 0, 2.0);
-    matrix.setCoefficient(0, 1, -2.0);
-    matrix.setCoefficient(0, 2, 12.0);
-    matrix.setCoefficient(1, 0, -3.0);
-    matrix.setCoefficient(1, 1, -12.5);
-    matrix.setCoefficient(2, 0, -6.1);
-
-    std::stringstream buffer;
-    std::streambuf * old = std::cout.rduf(buffer.rduf());
+    SpMV::SparseMatrix_COO<T> matrix = SpMV::SparseMatrix_COO<T>(3, 3);
+    matrix.setCoefficient(0, 0, T(2.0));
+    matrix.setCoefficient(0, 1, T(-2.0));
+    matrix.setCoefficient(0, 2, T(12.0));
+    matrix.setCoefficient(1, 0, T(-3.0));
+    matrix.setCoefficient(1, 1, T(-12.5));
+    matrix.setCoefficient(2, 0, T(-6.1));
 
     matrix.COO_view();
-    
-    std:: string text = buffer.str();
-    std::cerr << "The string is: " << text << std:endl;
 
-    std::string text_ref = "2.0 -2.0 12.0 \n -3.0 -12.5 0.0 \n -6.1 0.0 0.0";
+    int idx[6] = {0, 0, 0, 1, 1, 2};
+    int jdx[6] = {0, 1, 2, 0, 1, 0};
+    T rval[6] = {T(2.0), T(-2.0), T(12.0), T(-3.0), T(-12.5), T(-6.1)};
 
-    if (text == text_ref){
-        std::cerr << "Test passed" << std::endl;
-    } else {
-        std::cerr << "Test failed" << std::endl;
-        return 1;
+    string reference;
+    int k = 0;
+    int i = 0;
+
+    T value;
+    ifstream ViewResults("./COO.out");
+    while (getline(ViewResults, reference)){
+
+        int j = 0;
+        istringstream iss(reference);
+        while(iss >> value){
+            if (i==idx[k] && j==jdx[k]){
+                ASSERT_NEAR(value, T(rval[k]), T(1.E-6));
+                k++;
+            } else {
+                ASSERT_NEAR(value, T(0.), T(1.E-6));
+            }
+            j++;
+        }
+        i++;
     }
-
-    return 0;
 }
+
 
 
 // Create a test suite
@@ -83,5 +103,5 @@ main() -> int
   // Run the unit tests. If a test fails, the program will print failure info
   // and return 1.
   RUN_SUITE(my_suite<double>);
-  return 0; 
+  return 0;
 }

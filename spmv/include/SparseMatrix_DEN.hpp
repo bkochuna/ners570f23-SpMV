069a37d6ff8923a393c87163cc966c61b2f61bb2
Archana Sridhar <arsridha@gl-login3.arc-ts.umich.edu>
2023-10-26 10:32:09 -0400

    Fix header file

--- spmv/include/SparseMatrix_DEN.hpp
+++ spmv/include/SparseMatrix_DEN.hpp
@@ -12,8 +12,8 @@ namespace SpMV
     class SparseMatrix_DEN : public SparseMatrix<fp_type>
     {
     private:
-        //size_t *I    = nullptr;
-        //size_t *J    = nullptr; I and J should not be required? this is a dense matrix -Noah Zambrano (nzamb222)
+        size_t *I    = nullptr;
+        size_t *J    = nullptr;
         fp_type *A = nullptr;//nrows*ncols array storing the values of the dense matrix
 	//int nrows;
 	//std::map<std::pair<size_t, size_t>, fp_type> _buildCoeff;
@@ -22,8 +22,6 @@ namespace SpMV
     public:
         //default constructor
         SparseMatrix_DEN();
-        //destructor
-        ~SparseMatrix_DEN();
         
         // overloaded constructor
         SparseMatrix_DEN(const int nrows, const int ncols);
@@ -37,7 +35,7 @@ namespace SpMV
 	void setCoefficient_DEN(const size_t row, const size_t col, const fp_type aij); //Sets the coefficient at row and col to aij
         fp_type getCoef(const size_t i, const size_t j); //Returns the coefficient value at row and column indexes i and j
 	std::string getFormat(); //Returns the format (DEN) of the matrix
-    void DEN_view(fp_type* A);
+    void DEN_view();
 	// End DEN Accessor Functions
     };
 }

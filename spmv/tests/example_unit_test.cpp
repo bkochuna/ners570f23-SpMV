447b3f87546c0af8fd2c51ed833a45aec8ae64c4
Meet Manojkumar Patel <meetm@gl-login3.arc-ts.umich.edu>
2023-10-26 14:47:50 -0400

    unit test for DEN view method

--- spmv/tests/example_unit_test.cpp
+++ spmv/tests/example_unit_test.cpp
@@ -1,9 +1,13 @@
 #include <SpMV.hpp>
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector> // std::vector
 
 // Testing library required for testing (Always include this last!)
 #include "unit_test_framework.hpp"
+#include "SparseMatrix.hpp"
+#include "SparseMatrix_DEN.hpp"
 
 // User ASSERT(condition) to test if a condition is true.
 // Direct comparison of floating point numbers is not recommended, so we define
@@ -87,6 +91,121 @@ TEST_CASE(fixed_size_dot_product) {
   ASSERT(dot == soln);
 }
 
+// Create a DEN view unit test
+template <typename T>
+TEST_CASE(sample_mat) {
+  // size_t nrows = 5;
+  // size_t ncols = 5;
+  SpMV::SparseMatrix_DEN<double> *A = new SpMV::SparseMatrix_DEN<double>(5, 5);
+  A->setCoefficient(0, 2, 2.5);
+  A->setCoefficient(0, 3, 3.3);
+  A->setCoefficient(2, 2, 2.0);
+  A->setCoefficient(3, 2, 4.5);
+
+  // Prints DENSE matrix in .out format 
+  A->DEN_view();
+
+  //print a sample DENSE matrix in .out format
+  std::vector<std::vector<double>> matrix0 = {
+      {0.0, 0.0, 2.5, 3.3, 0.0},
+      {0.0, 0.0, 0.0, 0.0, 0.0},
+      {0.0, 0.0, 2.0, 0.0, 0.0},
+      {0.0, 0.0, 4.5, 0.0, 0.0}};
+
+  std::ofstream outputFile("DEN_ref.out");
+
+  if (!outputFile)
+  {
+    std::cerr << "Failed to create or open the file." << std::endl;
+  }
+
+  // Write the matrix to the file
+  outputFile << '[';
+  for (const auto &row : matrix0)
+  {
+    for (const double &value : row)
+    {
+      outputFile << value << ' ';
+    }
+    outputFile << '\n'; // Start a new line for each row
+  }
+  outputFile << ']';
+  outputFile.close();
+
+  std::cout << "Matrix has been written to 'DEN_ref.out'." << std::endl;
+
+  std::string filename1 = "DEN.out";
+  std::string filename2 = "DEN_ref.out";
+
+  std::vector<std::vector<double>> matrix1;
+  std::vector<std::vector<double>> matrix2;
+  std::ifstream inputFile1(filename1);
+
+  if (!inputFile1)
+  {
+    std::cerr << "Failed to open the file: "  << std::endl;
+    exit(1);
+  }
+
+  std::string line1;
+  while (std::getline(inputFile1, line1))
+  {
+    std::vector<double> row;
+    std::istringstream iss(line1);
+    double value;
+    while (iss >> value)
+    {
+      row.push_back(value);
+    }
+    matrix1.push_back(row);
+  }
+
+  inputFile1.close();
+  std::ifstream inputFile2(filename2);
+
+  if (!inputFile2)
+  {
+    std::cerr << "Failed to open the file: " << std::endl;
+    exit(1);
+  }
+
+  std::string line2;
+  while (std::getline(inputFile2, line2))
+  {
+    std::vector<double> row;
+    std::istringstream iss(line2);
+    double value;
+    while (iss >> value)
+    {
+      row.push_back(value);
+    }
+    matrix2.push_back(row);
+  }
+
+  inputFile2.close();
+
+  if (matrix1.size() != matrix2.size() || matrix1[0].size() != matrix2[0].size())
+  {
+    std::cout << "Matrices have different dimensions" << std::endl;
+    exit(1); // Matrices have different dimensions
+  }
+
+  for (size_t i = 0; i < matrix1.size(); ++i)
+  {
+    for (size_t j = 0; j < matrix1[i].size(); ++j)
+    {
+      if ((matrix1[i][j] - matrix2[i][j])<1e-4)
+      {
+        std::cout << "Matrices are not the same" << std::endl;
+        exit(1); // Matrices differ at this element
+      }
+    }
+  }
+
+  std::cout << "Matrices are same" << std::endl; // Matrices are equal
+}
+
+
 template <typename T>
 TEST_SUITE(add_sub_suite) {
   TEST(addition<T>);
@@ -98,6 +217,13 @@ TEST_SUITE(add_sub_suite) {
   TEST((fixed_size_dot_product<4, T>));
 }
 
+// Test suite for the Den view unit test
+template <typename T>
+TEST_SUITE(unit_test_suite) {
+  //Run the random matrix unit test (for now just testing 5x5 matrix)
+  TEST(sample_mat<T>);
+}
+
 auto
 main() -> int
 {
@@ -106,5 +232,6 @@ main() -> int
   RUN_SUITE(my_suite);
   RUN_SUITE(add_sub_suite<int>);
   RUN_SUITE(add_sub_suite<size_t>);
+  RUN_SUITE(unit_test_suite<double>);
   return 0; 
 }

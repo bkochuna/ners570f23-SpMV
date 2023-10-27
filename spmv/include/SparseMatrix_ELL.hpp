c9c5a5af52bd466759c5c56ba737044063e080d5
preetb1199 <147120931+preetb1199@users.noreply.github.com>
2023-10-27 18:54:37 -0400

    Update SparseMatrix_ELL.hpp
    Debugs

--- spmv/include/SparseMatrix_ELL.hpp
+++ spmv/include/SparseMatrix_ELL.hpp
@@ -13,16 +13,20 @@ namespace SpMV
         // size_t *J    = nullptr;
         size_t *colIdx = nullptr;
         fp_type *val = nullptr;
-        size_t _nrowsmax; // the method getLongestRow() will return _nrowsmax
-        string _format = "ELL";
+        size_t _nrowsmax = 0; // the method getLongestRow() will return _nrowsmax
+        std::string _format = "ELL";
         size_t getLongestRow();
     public:
         SparseMatrix_ELL(const int nrows, const int ncols);
-        void setCoefficient(const size_t row, const size_t col, const fp_type aij);
+        ~SparseMatrix_ELL();
+        void setCoefficient(const size_t row, const size_t col, const fp_type aij){
+		this -> setCoefficient(row, col, aij);
+	}
         void assembleStorage() {};
         fp_type getCoefficient(size_t i,size_t j);
         inline string getFormat() const { return _format; };
 	void matvec(fp_type* vecin, fp_type* vecout); //Added
+	void ELL_view();
 
     };
 }
